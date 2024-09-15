4e17c9625511bbbdd5aeeb29e93dd3bc6f144b0e
Divyansh Tiwari <divyansh.tiwari007@gmail.com>
2024-09-15 18:41:06 +0530

    learning started

---
+++ 001_intro.js
@@ -0,0 +1,17 @@
+// JavaScript
+
+// Javascript is a High-level, Object-oriented, Multi-paradigm, Interpreted programming languages.
+
+/* role of js in web-development */ 
+// HTML is the bones, CSS is the skin, and JavaScript is the muscles that make everything move.
+
+// HTML is the structure, CSS is the presentation (the style), and JavaScript is the engine that brings life by introducing real-time interaction.
+
+// Example : Form validation with real-time feedback before submitting the data to the server
+// HTML can define the structure of a form, and CSS can style it, but neither can check if the user has entered the correct data (e.g., valid email, password length) in real time. JavaScript is necessary to provide immediate feedback, guiding users to correct mistakes as they fill out the form.
+
+// JavaScript Releases :
+// ES5 -> ES6 (ES2015) -> ES7 (ES2016) -> ES8 (ES2017) -> ES9 (ES2018) -> ES10 (ES2019) -> ES11 (ES2020) -> ES12 (ES2021) -> ES13 (ES2022) -> ES14 (ES2023)
+
+// ES6 was the biggest updated and all version after and including this are know as morden javascript
+
