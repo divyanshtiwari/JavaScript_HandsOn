9392037177faa1fcc0e70f45c92ed3435660c531
Divyansh Tiwari <divyansh.tiwari007@gmail.com>
2024-09-18 07:36:23 +0530

    add values and variables

---
+++ 002_valuesAndVariables.js
@@ -0,0 +1,24 @@
+// values and variables
+// values are the data we want to store, display, manipulate etc.
+// variables are the storage buckets with a lable attached that can be used to recall a value stored in that bucket.
+
+console.log('MyName'); // 'MyName' here is a value (specifically string value)
+
+// variable declaration
+let firstName = "divyansh"; // firsName is the variable (lable) and "divyansh" is the value that we are storing in that variable.
+
+console.log(firstName); // here we are using the variable to recall the value stored inside.
+// Note : whenever a variable is encountred it gets replaced my its value and that's what will be printed out.
+
+// nameing conventions for variables
+// camelCase : firstName (I prefer this)
+// snake_case : first_name
+
+// a variable name in js can only have letters, number, underscore and dollrs
+// numbers at the start of a variable name is illegal in js
+// keywords are also not allowed like : new, function, etc.
+// keep your variables descript enough but not super long
+
+let myFirstJob = 'programmer';
+
+console.log(myFirstJob)
