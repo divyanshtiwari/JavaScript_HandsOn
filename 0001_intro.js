39eb199669e6154734d968a32cb1546e64b04890
Divyansh Tiwari <divyansh.tiwari007@gmail.com>
2025-04-06 00:22:13 +0530

    first commit with intro and basic structure

---
+++ 0001_intro.js
@@ -0,0 +1,21 @@
+// JavaScript.
+
+// What is JavaScript and how does it fits in the world of programming ?
+
+// JavaScript officially know as Ecma-Script is a high-level, dynamic, object-oriented, interpreted, multi-paradigm programming language.
+
+/* role of javaScript in web-development */
+// HTML is the skeleton, css is the skin, and JavaScript is the muscle that moves it all.
+
+
+// HTML is the structure, CSS is the presentation (the style), and JavaScript is the engine that brings life by introducing real-time interaction.
+
+// Example : Form validation with real-time feedback before submitting the data to the server
+// HTML can define the structure of a form, and CSS can style it, but neither can check if the user has entered the correct data (e.g., valid email, password length) in real time. JavaScript is necessary to provide immediate feedback, guiding users to correct mistakes as they fill out the form.
+
+// HTML builds the form, CSS makes it look nice, but none of them can tell the user that you have entered a duplicate email, here you’ll need JavaScript. It adds the brains to the form—handling things like validation, interactivity, and talking to the server if needed.
+
+// JavaScript Releases :
+// ES5 -> ES6 (ES2015) -> ES7 (ES2016) -> ES8 (ES2017) -> ES9 (ES2018) -> ES10 (ES2019) -> ES11 (ES2020) -> ES12 (ES2021) -> ES13 (ES2022) -> ES14 (ES2023) -> ES15 (ES2024)
+
+// ES6 was the biggest updated and all version after and including this are know as morden javascript
